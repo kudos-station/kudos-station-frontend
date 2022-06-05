d9fbe16826ec4842fe4042a20e294021d1346365
Irmak Erkol <ierkol18@ku.edu.tr>
2022-06-06 01:28:21 +0300

    usersByProject as table done

--- src/components/FilterPage/usersByProject.component.js
+++ src/components/FilterPage/usersByProject.component.js
@@ -21,18 +21,32 @@ function UsersByProject() {
     var currentLastNames = JSON.parse(location.state.lastNames)
 
     
-   
     return (
         <>
 
-            <div className="usersByDepartmentComponent">
-                <div id="users" >
+            <div className="main-page-component">
+                <div className="container" >
+                    <h3 className="p-3 text-center"></h3>
                     <h1 className="text-center"> {location.state.projectName} </h1>
                     <p> These users are working on the input project and are recieved all of the kudos types and sent at least one kudos type.</p>
-                    <div id="horizontalLine"  ></div> 
-                        {currentUsers.map(currentUsers => <p> {currentUsers}</p>)} 
-                        {currentFirstNames.map(currentFirstNames => <p> {currentFirstNames}</p>)} 
-                        {currentLastNames.map(currentLastNames => <p> {currentLastNames}</p>)} 
+                    <table className="table table-striped table-bordered" style={{ "marginTop": "15px" }}>
+                        <thead>
+                            <tr>
+                                <th>Username</th>
+                                <th>FirstName</th>
+                                <th>LastName</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {currentUsers && currentUsers.map((kudo, index) =>
+                                <tr key={index}>
+                                    <td>{currentUsers[index]}</td>
+                                    <td>{currentFirstNames[index]}</td>
+                                    <td>{currentLastNames[index]}</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
                 </div>
 
             </div>
